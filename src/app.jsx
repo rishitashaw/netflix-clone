3b6039385ef037b462caabf5f53b66cc3df4e4fe
Theseregrets <rishitashaw49@gmail.com>
2021-05-15 14:42:49 +0530

    footer done

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { FooterContainer } from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
