725a1a65c41c3fc03b5d0e55785a1d70121e3fec
Theseregrets <rishitashaw49@gmail.com>
2021-07-02 18:41:12 +0530

    helper added

---
+++ src/helpers/routes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) return children;
+
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+        return null;
+      }}
+    />
+  );
+}
