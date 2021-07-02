e2bfd4e407527ef2f448967655d349c959fb8379
Theseregrets <rishitashaw49@gmail.com>
2021-07-02 19:46:24 +0530

    browser page building

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { useContent } from "../hooks";
 
-export default function Browse({ children, ...restProps }) {
-  return <>hello mf</>;
+export default function Browse() {
+  //series and films
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+
+  //slides
+  //pass to browse container
+  return <></>;
 }
