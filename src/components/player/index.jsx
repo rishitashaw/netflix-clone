f2fe9fdaee0b9ce337b14ebc8a8c39721f0d42ac
Theseregrets <rishitashaw49@gmail.com>
2021-07-03 10:55:45 +0530

    player added

---
+++ src/components/player/index.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useContext, createContext } from "react";
+import ReactDOM from "react-dom";
+import { Container, Button, Overlay, Inner, Close } from "../styles/player";
+
+export const PlayerContext = createContext();
+
+export default function Player({ children, ...restProps }) {
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  return (
+    <PlayerContext.Provider value={{ showPlayer, setShowPlayer }}>
+      <Container {...restProps}>{children}</Container>
+    </PlayerContext.Provider>
+  );
+}
+
+Player.Video = function PlayerVideo({ src, ...restProps }) {
+  const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+
+  return showPlayer
+    ? ReactDOM.createPortal(
+        <Overlay onClick={() => setShowPlayer(false)}>
+          <Inner>
+            <video id="netflix-player" controls>
+              <source src={src} type="video.mp4" />
+            </video>
+          </Inner>
+        </Overlay>
+      )
+    : null;
+};
